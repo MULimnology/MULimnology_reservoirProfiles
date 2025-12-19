4b52537b9fc80a3195b47254c0e84bcce1a327b4
richardson-david <richardsond@newpaltz.edu>
2025-12-19 14:51:02 -0500

    Updating the export for Thin Ice profiles from Level 2 data.

---
+++ 05_Scripts/05.02_MULimnology_subsettingLevel02data_forThinIce.R
@@ -0,0 +1,94 @@
+##Script 05.02: Loading and selecting specific level02 profiles for Thin Ice####
+##Created 19Dec2025 by David Richardson (hereafter DCR)
+##This loads level 2 data from all the years, then subsets specific profiles for Thin Ice####
+
+#Libraries
+if (!require(tidyverse)) {install.packages("tidyverse")}
+if (!require(lubridate)) {install.packages("lubridate")}
+if (!require(stringr)) {install.packages("stringr")}
+
+#Load packages
+library(tidyverse)
+library(lubridate)
+library(stringr)
+
+#Run functions script to upload all the user defined functions####
+source("05_Scripts/00_MULimnology_reservoirProfileQAQC_Functions.R")
+
+#*Set the directory path here####
+dirPath<-paste0("02_Level2_Data/")
+
+#Identify all the individual .csv files####
+Level2_files<-list.files(dirPath,pattern = "*.csv")
+
+#Initialize storage location####
+List_Level2<-list()
+
+#Loop through all the level3 files and load them individually####
+#debug fileIndex<-1
+#This goes from 8 to 9 - 2024 and 2025, expand out for next year
+for(fileIndex in 8:9){
+  List_Level2[[fileIndex]]<-read_csv(file=paste0(dirPath,Level2_files[fileIndex]), col_types = cols()) #last argument suppresses the message on input about column types, helpful for mass upload
+}
+
+#Bind them all together####
+Level2_allData<-do.call(bind_rows, List_Level2)
+
+#List of all MULakeNumbers that are in the Thin Ice project
+ThinIce_MULakeNumbers<-c("438","088","219","440","112","446","274")
+
+#Filter out only thin ice lakes then split into a list for each profile####
+ThinIceSplit<-Level2_allData%>%
+  filter(MULakeNumber%in%ThinIce_MULakeNumbers)%>%
+  group_by(MULakeNumber,date)%>%
+  group_split()
+
+#Loop through all the profiles####
+#ThinIceProfile.index<-1
+for(ThinIceProfile.index in 1:length(ThinIceSplit)){
+  temp.df<-ThinIceSplit[[ThinIceProfile.index]]
+  #Get out the MULakeNumber####
+  MULakeNumber<-temp.df$MULakeNumber[1]
+  
+  #Create a file path
+  dir.output<-paste0("06_Outputs/ThinIce/Level2_Profiles_",MULakeNumber)
+  
+  # Check if the folder exists
+  if (!file.exists(paste0(dir.output))) {
+    # If the file does not exist, create it
+    dir.create(paste0(dir.output))
+  }
+  
+  #Generate the file name
+  year<-year(temp.df$date)[1]
+  month<-sprintf("%02d",month(temp.df$date)[1])
+  day<-sprintf("%02d",day(temp.df$date)[1])
+    #*File name####
+    file.name<-paste(MULakeNumber,year,month,day,"profile.csv",sep="_")
+  #Paste out the profile  
+  write_csv(x=temp.df,file=paste(dir.output,file.name,sep="/"))
+  
+}
+
+
+########################Figure out the distributions of the depths for each lake####################
+#Get teh unique list of sites####
+uniqueLakeNumbers<-sort(unique(Level3_allData$MULakeNumber))
+
+#Lake.index=1
+#Export a histogram of the maxdepth for each profile by site as a page####
+pdf(paste0("06_Outputs/Level3_MaxDepth_plots.pdf"), onefile = TRUE,width=8.5,height=11)
+
+#Go through all the lakes####
+#lake.index=1
+for(lake.index in 1:length(uniqueLakeNumbers)){
+  temp<-Level3_allData%>%filter(MULakeNumber==uniqueLakeNumbers[lake.index]) #Extract the data just for that lake
+  gg.temp<-ggplot(data=temp,aes(x=maxDepth_m))+geom_histogram()+labs(title=paste0("MULakeNumber: ",uniqueLakeNumbers[lake.index])) #create the histogram plot####
+  print(gg.temp)
+}
+
+dev.off()
+
+#Check 149####
+Level3_allData%>%filter(MULakeNumber%in%uniqueLakeNumbers[which(substr(uniqueLakeNumbers,1,3)=="149")])%>%print(n=Inf)
+####
