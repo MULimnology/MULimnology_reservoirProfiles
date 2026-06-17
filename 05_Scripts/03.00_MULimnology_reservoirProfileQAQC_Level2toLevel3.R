ccd9892262b3b1886457f36c39d520343b6783c7
richardson-david <richardsond@newpaltz.edu>
2026-06-17 15:41:10 -0400

    Running YSI profiles 2026 current through 17Jun Level 01-03.

--- 05_Scripts/03.00_MULimnology_reservoirProfileQAQC_Level2toLevel3.R
+++ 05_Scripts/03.00_MULimnology_reservoirProfileQAQC_Level2toLevel3.R
@@ -30,8 +30,8 @@ source("05_Scripts/00_MULimnology_reservoirProfileQAQC_Functions.R")
 
 #Read in level 2 files from a particular year####
 #Set years here, update each year here####
-yearIndex<-2022
-  #possible years: c("Historical","2017","2018","2019","2020","2021","2022","2023","2024","2025")
+yearIndex<-2026
+  #possible years: c("Historical","2017","2018","2019","2020","2021","2022","2023","2024","2025","2026")
 
 #*Set the directory path here####
 dirPath<-paste0("02_Level2_Data/")
